198c85879cf4f49f084e72cc65f633d574e3e26a
[aryan] <[aryanarora3011@gmail.com]>
2024-06-07 16:01:57 +0530

    first commit

---
+++ template_project/basicapp/templatetags/my_extras.py
@@ -0,0 +1,13 @@
+from django import template
+
+register= template.Library()
+
+@register.filter(name="cutout")
+def cutout(value,arg):
+     """
+        This cuts out all  values of "arg" from the string!
+     """
+     return value.replace(arg,'')
+
+
+# register.filter('cutout',cutout)
